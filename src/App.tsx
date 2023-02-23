6a6b35ff484260dd9914f188c988082875667198
jesnagbg <j.m.pettersson@hotmail.se>
2023-02-23 21:33:43 +0100

    with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import "./App.css";
+import { Button } from './components/Button';
 
 interface AstrologyData {
   date_range: string;
   description: string;
 }
 
-
 function App() {
   const [astrologyData, setAstrologyData] = useState<AstrologyData | null>(null);
 
-  useEffect(() => {
+  const fetchAstrologyData = async (sign: string) => {
     const options: AxiosRequestConfig = {
       method: 'POST',
       url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
-      params: { sign: 'aquarius', day: 'today' },
+      params: { sign, day: 'today' },
       headers: {
         'X-RapidAPI-Key': 'b3616ca703msh7f91a1b391d3b37p14580cjsn9dd28c3d9990',
         'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com',
       },
     };
 
-    const fetchData = async () => {
-      try {
-        const response: AxiosResponse<AstrologyData> = await axios.request(options);
-
-        setAstrologyData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
+    try {
+      const response: AxiosResponse<AstrologyData> = await axios.request(options);
+      setAstrologyData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   if (!astrologyData) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <Button onClick={() => fetchAstrologyData('aquarius')}>Aquarius</Button>
+        <Button onClick={() => fetchAstrologyData('cancer')}>Cancer</Button>
+      </div>
+    );
   }
 
   return (
