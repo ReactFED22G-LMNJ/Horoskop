66697742b47a360bc1775c8674b9dda9cf0833e6
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-02-24 14:57:42 +0100

    outcommented code we'll use later but not now

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// const router = createRoutesFromElement(
+//   <Route>
+//     <Route path="/" element={}/> 
+//     <Route path="zodiac" element={}/> 
+//     <Route path="main" element={}>
+//       <Route paht=":day" element={} />
+//     </Route> 
+//     <Route path="about" element={}/> 
+//   </Route>
+// )
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App/>
+    {/* <RouterProvider router={router}/> */}
   </React.StrictMode>,
 )
