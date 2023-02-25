ed0e41733e1c551949a5b661d2b1d71ab51c78ba
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-02-26 00:07:46 +0100

    added first code for our drop down menu

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import TriangleArrow from '/assets/triangle-arrow.png';
+
+const ZodiacDropdown = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedZodiac, setSelectedZodiac] = useState<string | null>(null);
+
+  const handleZodiacSelect = (zodiac: string) => {
+    setSelectedZodiac(zodiac);
+    setShowDropdown(false);
+  };
+
+  const zodiacs = [
+    'Aries',
+    'Taurus',    
+    'Gemini',    
+    'Cancer',    
+    'Leo',    
+    'Virgo',    
+    'Libra',    
+    'Scorpio',    
+    'Sagittarius',    
+    'Capricorn',    
+    'Aquarius',    
+    'Pisces'
+    ];
+
+  return (
+    <Dropdown>
+      <DropdownButton onClick={() => setShowDropdown(!showDropdown)}>
+        {selectedZodiac || 'Zodiac Sign'}
+        <TriangleArrowIcon src={TriangleArrow} alt="arrow down icon"/>
+      </DropdownButton>
+      <DropdownContent show={showDropdown}>
+        {zodiacs.map((zodiac: string) => (
+            <ZodiacSign key={zodiac} onClick={() => handleZodiacSelect(zodiac)}>
+                {zodiac}
+          </ZodiacSign>
+        ))}
+      </DropdownContent>
+    </Dropdown>
+  );
+};
+
+export default ZodiacDropdown;
+
+
+const Dropdown = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const DropdownButton = styled.button`
+  background-color: #ffffff;
+  color: #000000;
+  padding: 0.5rem;
+  font-size: 1rem;
+  cursor: pointer;
+  font-family: 'Tenor Sans', sans-serif;
+  border: 0.08rem solid;
+  border-radius: 0.5rem;
+  width: 10rem;
+  text-align: left;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TriangleArrowIcon = styled.img`
+    height: 1.2rem;
+`;
+
+const DropdownContent = styled.div`
+  display: ${(props: { show: boolean }) => (props.show ? 'block' : 'none')};
+  position: absolute;
+  z-index: 1;
+  background-color: #ffffff;
+  min-width: 10rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.2);
+  padding: 0.75rem 1rem;
+  margin-top: 0.5rem;
+  font-family: 'Tenor Sans', sans-serif;
+`;
+
+const ZodiacSign = styled.div`
+  padding: 0.4rem;
+  &:hover {
+    background-color: #F6F3EF;
+    border-radius: 0.5rem;
+  }
+`;
+
