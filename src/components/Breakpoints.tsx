69d249435330295e1e095a02764605c2c06aefe0
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-02-27 13:17:25 +0100

    added Breakpoints for responsiveness

---
+++ src/components/Breakpoints.tsx
@@ -0,0 +1,26 @@
+const size = {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    mobileXL: '524px',
+    mobileXXL: '592px',
+    tablet: '768px',
+    tabletXL: '809px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktop: '2560px'
+}
+
+export const device = {
+    mobileS: `(max-width: ${size.mobileS})`,
+    mobileM: `(max-width: ${size.mobileM})`,
+    mobileL: `(max-width: ${size.mobileL})`,
+    mobileXL: `(max-width: ${size.mobileXL})`,
+    mobileXXL: `(max-width: ${size.mobileXXL})`,
+    tablet: `(max-width: ${size.tablet})`,
+    tabletXL: `(max-width: ${size.tabletXL})`,
+    laptop: `(max-width: ${size.laptop})`,
+    laptopL: `(max-width: ${size.laptopL})`,
+    desktop: `(max-width: ${size.desktop})`,
+    desktopL: `(max-width: ${size.desktop})`
+};
