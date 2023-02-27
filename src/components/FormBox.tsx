bb094039077ecae95aa5a8c9d3e01d677fd9cbf0
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-02-27 21:10:04 +0100

    changed styled.div to styled.form

--- src/components/FormBox.tsx
+++ src/components/FormBox.tsx
@@ -149,7 +149,7 @@ const FormContainer = styled.div`
     align-items: center;
 `;
 
-const Form = styled.div`
+const Form = styled.form`
     background-color: white;
     font-family: 'Tenor Sans', sans-serif;
     box-shadow: 0px 6px 5px 0px rgba(0,0,0,0.37);
