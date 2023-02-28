6312757b8517560a72d672929bd7c47be9d4bd8a
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-02-28 10:32:47 +0100

    added handleSubmit to formbox

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,12 +1,16 @@
-import FormBox from "../components/FormBox";
 import { Link } from "react-router-dom";
+import FormBox from "../components/FormBox";
 import HeaderStartPage from "../components/HeaderStartPage";
+
+const handleSubmit = (name: string) => {
+    console.log(`The submitted name is: ${name}`);
+};
 
 function StartPage() {
     return (
         <div>
             <HeaderStartPage />
-            <FormBox />
+            <FormBox onSubmit={handleSubmit} />
             <Link to="chooseyoursign">Choose your sign</Link> 
         </div>
     );
