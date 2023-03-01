8c9e7956080b4d008ac8cc0f5b67c3cff030c9d6
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-01 09:28:28 +0100

    added styling for navbar

--- src/components/HeaderDailyHoroscope.tsx
+++ src/components/HeaderDailyHoroscope.tsx
@@ -33,7 +33,9 @@ const Container = styled.header`
     justify-content: center;
     padding-top: 1.4rem;
     padding-bottom: 1.4rem;
-    margin-bottom: 1.8rem;
+    -webkit-box-shadow: 0 0.625rem 0.375rem -0.375rem rgba(0,0,0,0.37);
+    -moz-box-shadow: 0 0.625rem 0.375 -0.375rem rgba(0,0,0,0.37);
+    box-shadow: 0 0.625rem 0.375 -0.375rem rgba(0,0,0,0.37);
 
     @media ${device.mobileXL} {
       flex-direction: column;
@@ -63,7 +65,8 @@ const DropDownContainer = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
-    padding-bottom: 2.4rem;
+    align-items: center;
+    padding-bottom: 2rem;
 
     @media ${device.laptop} {
       padding-bottom: 0.6rem;
