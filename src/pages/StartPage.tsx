1d7fe642364907cfe4f63179ee118b8978adab0a
jesnagbg <j.m.pettersson@hotmail.se>
2023-03-01 15:16:47 +0100

    more errb

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,11 +1,16 @@
 import FormBox from "../components/FormBox";
 import HeaderStartPage from "../components/HeaderStartPage";
+import ErrorBoundary from "../ErrorBoundry";
 
 function StartPage() {
     return (
         <div>
+            <ErrorBoundary>
             <HeaderStartPage />
+            </ErrorBoundary>
+            <ErrorBoundary>
             <FormBox />
+            </ErrorBoundary>
         </div>
     );
 }
