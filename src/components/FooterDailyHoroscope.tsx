6b0a182e0f16a8a80f6806c56f84b1be8975775f
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-01 16:16:38 +0100

    Added footer component with css

--- src/components/FooterDailyHoroscope.tsx
+++ src/components/FooterDailyHoroscope.tsx
@@ -1,25 +1,27 @@
 import styled from "styled-components";
 import { Button } from "./Button";
 
-function Footer() {
+function FooterDailyHoroscope() {
     return(
     <FooterContainer>
         <Button to="/">Back</Button>
+        <Button to="/chooseyoursign">All Zodiac Signs</Button>
     </FooterContainer>
     );
 }
 
-export default Footer;
+export default FooterDailyHoroscope;
 
 
 const FooterContainer = styled.footer`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-wrap: wrap;
     -webkit-box-shadow: 0.063rem -0.125rem 0.313rem 0 rgba(0,0,0,0.37);
     -moz-box-shadow: 0.063rem -0.125rem 0.313rem 0 rgba(0,0,0,0.37);
     box-shadow: 0.063rem -0.125rem 0.313rem 0 rgba(0,0,0,0.37);
-    height: 4rem;
+    height: 5rem;
     margin-top: 1.8rem;
 
 `;
